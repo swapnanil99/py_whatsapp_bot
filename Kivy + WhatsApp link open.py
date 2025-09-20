4cb573c1076e9b58626f274493561ed9936127c2
Swapnanil Maity <73428566+swapnanil99@users.noreply.github.com>
2025-09-21 00:16:06 +0530

    Update and rename main.py to Kivy + WhatsApp link open.py

--- Kivy + WhatsApp link open.py
+++ Kivy + WhatsApp link open.py
@@ -5,16 +5,15 @@ import webbrowser
 class MyApp(App):
     def build(self):
         return Button(
-            test_button="Test Button",
+            text="Test Button",
             on_press=self.send_message,
         )
+
     def send_message(self, instance):
-        phone_number = "919002394195"
+        phone_number = "replace this area with your friends phone number "
         message = "Hello from my bot!"
         url = f"https://wa.me/{phone_number}?text={message.replace(' ', '%20')}"
         webbrowser.open(url)
 
-
 if __name__ == "__main__":
     MyApp().run()
-    
